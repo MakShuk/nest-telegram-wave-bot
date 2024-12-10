7d9593eaf28a7a63736ddc976d5018e5f34ee874
Maksim Shuklin <makshuk@bk.ru>
2024-12-10 12:16:15 +0300

    refactor: improve code formatting and consistency in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,7 @@ export class AppService implements OnModuleInit {
     private readonly notificationService: NotificationService,
     @Inject('TELEGRAM_BOT_INSTANCE')
     private readonly bot: Telegraf<Context>,
-  ) { }
+  ) {}
   getStatus(): string {
     return 'OK';
   }
@@ -34,38 +34,52 @@ export class AppService implements OnModuleInit {
       const stop = Markup.button.callback(`Остановить`, 'stop');
       await ctx.reply(
         `Интервал запуска уведомлений`,
-        Markup.inlineKeyboard([time3Min, time5Min, time10Min, time15Min, stop], { columns: 2 }),
+        Markup.inlineKeyboard(
+          [time3Min, time5Min, time10Min, time15Min, stop],
+          { columns: 2 },
+        ),
       );
-    })
+    });
 
     this.telegramActionsService.buttonAction('time3Min', async (ctx) => {
-      this.notificationService.stopNotification()
-      this.notificationService.startNotification(() => this.sendNotification('Меняй'), 3)
-      ctx.reply('Уведомления запущены с интервалом 3 минуты')
-    })
+      this.notificationService.stopNotification();
+      this.notificationService.startNotification(
+        () => this.sendNotification('Меняй'),
+        3,
+      );
+      ctx.reply('Уведомления запущены с интервалом 3 минуты');
+    });
 
     this.telegramActionsService.buttonAction('time5Min', async (ctx) => {
-      this.notificationService.stopNotification()
-      this.notificationService.startNotification(() => this.sendNotification('Меняй'), 5)
-      ctx.reply('Уведомления запущены с интервалом 5 минут')
-    })
+      this.notificationService.stopNotification();
+      this.notificationService.startNotification(
+        () => this.sendNotification('Меняй'),
+        5,
+      );
+      ctx.reply('Уведомления запущены с интервалом 5 минут');
+    });
 
     this.telegramActionsService.buttonAction('time10Min', async (ctx) => {
-      this.notificationService.stopNotification()
-      this.notificationService.startNotification(() => this.sendNotification('Меняй'), 10)
-      ctx.reply('Уведомления запущены с интервалом 10 минут')
-    })
+      this.notificationService.stopNotification();
+      this.notificationService.startNotification(
+        () => this.sendNotification('Меняй'),
+        10,
+      );
+      ctx.reply('Уведомления запущены с интервалом 10 минут');
+    });
 
     this.telegramActionsService.buttonAction('time15Min', async (ctx) => {
-      this.notificationService.stopNotification()
-      this.notificationService.startNotification(() => this.sendNotification('Меняй'), 15)
-      ctx.reply('Уведомления запущены с интервалом 15 минут')
-    })
+      this.notificationService.stopNotification();
+      this.notificationService.startNotification(
+        () => this.sendNotification('Меняй'),
+        15,
+      );
+      ctx.reply('Уведомления запущены с интервалом 15 минут');
+    });
 
     this.telegramActionsService.buttonAction('stop', async (ctx) => {
-      this.notificationService.stopNotification()
-      ctx.reply('Уведомления остановлены')
-    })
+      this.notificationService.stopNotification();
+      ctx.reply('Уведомления остановлены');
+    });
   }
-
 }
