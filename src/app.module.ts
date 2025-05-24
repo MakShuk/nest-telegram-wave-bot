b45340e31a8a898c9011d851463f46cada7a68ac
Your Name <your.email@example.com>
2025-05-24 07:00:48 +0300

    refactor: remove duplicate TelegramActionsService from AppModule and update TelegramActionsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { getConfigModuleOptions } from 'src/configs/config/config.module';
 import { TelegramModule } from 'src/configs/telegram.config';
 import { TelegramActionsModule } from './telegram-actions/telegram-actions.module';
-import { TelegramActionsService } from './telegram-actions/telegram-actions.service';
 import { NotificationModule } from './notification/notification.module';
 import { ShutdownService } from './services/shutdown.service';
 import { IntervalTaskService } from './services/interval-task.service';
@@ -13,6 +12,6 @@ import { IntervalTaskService } from './services/interval-task.service';
 @Module({
   imports: [ConfigModule.forRoot(getConfigModuleOptions()), TelegramModule.forRootAsync(), TelegramActionsModule, NotificationModule],
   controllers: [AppController],
-  providers: [AppService, TelegramActionsService, IntervalTaskService, ShutdownService],
+  providers: [AppService, IntervalTaskService, ShutdownService],
 })
 export class AppModule { }
--- src/telegram-actions/telegram-actions.module.ts
+++ src/telegram-actions/telegram-actions.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TelegramActionsService } from './telegram-actions.service';
 
 @Module({
-  providers: [TelegramActionsModule],
+  providers: [TelegramActionsService],
+  exports: [TelegramActionsService],
 })
 export class TelegramActionsModule {}
